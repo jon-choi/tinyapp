8d95e697bcdc049e9525d4ce24c00ebd22092777
Jonathan Choi <jonchoi11@gmail.com>
2021-08-04 02:07:17 +0000

    fixed new/urls route

--- express_server.js
+++ express_server.js
@@ -27,7 +27,10 @@ app.get('/', (req, res) => {
 });
 
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+  const templateVars = {
+    username: req.cookies['username']
+  }
+  res.render('urls_new', templateVars);
 });
 
 app.get("/urls", (req, res) => {
