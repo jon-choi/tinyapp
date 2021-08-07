1c965669e0f334f6b606bdd73c2e4e7ffde005c9
Jonathan Choi <jonchoi11@gmail.com>
2021-08-07 02:00:07 +0000

    short link now direct user to long urls

--- express_server.js
+++ express_server.js
@@ -67,10 +67,15 @@ app.get("/", (req, res) => {
 
 // redirects from short url to long url
 app.get("/u/:shortURL", (req, res) => {
-  const shortURL = req.params.shortURL;
-
-  if (!urlDatabase[shortURL]) {
-    res.send("We can't find the URL you're searching for.");
+  if (urlDatabase[req.params.shortURL]) {
+    const longURL = urlDatabase[req.params.shortURL].longURL;
+    if (longURL === undefined) {
+      res.status(302);
+    } else {
+      res.redirect(longURL);
+    }
+  } else {
+    res.status(404).send("The short URL you're trying to access does not exist, apparently.");
   }
 });
 
@@ -198,7 +203,7 @@ app.post("/logout", (req, res) => {
 });
 
 // route to redirect to urls page
-app.post("/urls", (req, res) => {
+app.post("/urls/", (req, res) => {
   if (req.session.user_id) {
     const shortURL = generateRandomString();
     urlDatabase[shortURL] = {
@@ -225,16 +230,19 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 });
 
 // updates url resource
-app.post("/urls/", (req, res) => {
-  const userID = req.session.user_id;
-  const checkUrl = urlsForUser(userID, urlDatabase);
+app.post("/urls/:shortURL", (req, res) => {
+  if (req.session.user_id) {
+    const longURL = req.body.longURL;
+    const shortURL = req.params.shortURL;
 
-  if (Object.keys(checkUrl).includes(req.params.id)) {
-    const shortURL = req.params.id;
-    urlDatabase[shortURL].longURL = req.body.shortURL;
-    res.redirect('/urls');
+    if (urlDatabase[shortURL].userID === req.session.user_id) {
+      urlDatabase[shortURL].longURL = longURL;
+      res.redirect('/urls');
+    } else {
+      res.send("You don't own this URL. Get out.");
+    }
   } else {
-    res.send("You're not allowed to edit this url.")
+    res.redirect('/login');
   }
 });
 
